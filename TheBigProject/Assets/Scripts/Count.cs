4b3ae54cd5b90920005051d8b89b7f3ce75d505e
uchihanaruto9371 <uchihanaruto9371@gmail.com>
2018-03-13 11:23:48 +0200

    Add some questions

---
+++ TheBigProject/Assets/Scripts/Count.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.UI;
+
+public class Count : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int i = 0;
+        int n = 100000000;
+        while (i < n)
+        {
+            ++i;
+        }
+        if (i >= n)
+        {
+            SceneManager.LoadScene(0);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TheBigProject/Assets/Scripts/Scenechanger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Scenechanger : MonoBehaviour {
+    public void change(int scene)
+    {
+        Application.LoadLevel(scene);
+    }
+	
+}
